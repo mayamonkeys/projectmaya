f0fd40aca3867e8fb94385d1b656c62b17ee9f0b
Daniel J. Hofmann <daniel@trvx.org>
2012-06-05 17:13:22 +0200

    Fixed luabridge for clang and gcc

--- extern/LuaBridge/LuaBridge.h
+++ extern/LuaBridge/LuaBridge.h
@@ -861,12 +861,13 @@
 namespace luabridge
 {
 
-#if defined (__APPLE_CPP__) || defined(__APPLE_CC__)
-// Do not define THROWSPEC since the Xcode compilers do not distinguish
+#if defined (__APPLE_CPP__) || defined(__APPLE_CC__) || defined(__clang__) || defined(__GNUC__)
+// Do not define THROWSPEC since the specific compilers do not distinguish
 // the throw specification in the function signature.
 #else
 #define THROWSPEC throw()
 #endif
+
 
 //==============================================================================
 /**
